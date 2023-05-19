5ad83f7e3b9925c968b957ee99db205b5fe73ee4
Bernadett Urban <bernadett.urban@outlook.com>
2023-05-19 15:09:12 +0200

    chore: initial files

---
+++ otp-mobil/src/main/java/com/example/otpmobil/OtpMobilApplication.java
@@ -0,0 +1,13 @@
+package com.example.otpmobil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtpMobilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtpMobilApplication.class, args);
+    }
+
+}
---
+++ otp-mobil/src/test/java/com/example/otpmobil/OtpMobilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.otpmobil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtpMobilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
