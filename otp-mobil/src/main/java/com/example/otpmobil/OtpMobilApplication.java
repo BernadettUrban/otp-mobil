c79acd4c23c9a108d407af86a3bab4d6cc8a67fe
Bernadett Urban <bernadett.urban@outlook.com>
2023-05-19 16:40:35 +0200

    feat: customers printed on restart

--- otp-mobil/src/main/java/com/example/otpmobil/OtpMobilApplication.java
+++ otp-mobil/src/main/java/com/example/otpmobil/OtpMobilApplication.java
@@ -1,13 +1,31 @@
 package com.example.otpmobil;
 
+import com.example.otpmobil.model.Customer;
+import com.opencsv.bean.CsvToBeanBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.FileReader;
+import java.util.List;
 
 @SpringBootApplication
 public class OtpMobilApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
         SpringApplication.run(OtpMobilApplication.class, args);
+
+        String fileName ="src\\main\\resources\\static\\csv_input\\customer.csv";
+
+        List<Customer> beans = new CsvToBeanBuilder(new FileReader(fileName))
+                .withType(Customer.class)
+                .withSeparator(';')
+                .build()
+                .parse();
+
+        beans.forEach(System.out::println);
+
+
     }
 
 }
