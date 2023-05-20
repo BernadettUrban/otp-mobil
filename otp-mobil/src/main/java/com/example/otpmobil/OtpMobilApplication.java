8a5742d59343c6cda87c8ba3c7e069373a71d5a8
Bernadett Urban <bernadett.urban@outlook.com>
2023-05-20 16:15:43 +0200

    feat: logger added

--- otp-mobil/src/main/java/com/example/otpmobil/OtpMobilApplication.java
+++ otp-mobil/src/main/java/com/example/otpmobil/OtpMobilApplication.java
@@ -1,27 +1,15 @@
 package com.example.otpmobil;
 
-import com.example.otpmobil.services.CustomerServiceImpl;
-import com.example.otpmobil.services.LoggerService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OtpMobilApplication implements CommandLineRunner {
-    //private final static Logger logger = Logger.getLogger(OtpMobilApplication.class);
-    private final LoggerService loggerService;
-
-    public OtpMobilApplication(LoggerService loggerService, CustomerServiceImpl customerService) {
-        this.loggerService = loggerService;
-
+public class OtpMobilApplication {
+    static {
+        System.setProperty("logback.configurationFile", "otp-mobil\\config\\logback\\logback.xml");
     }
 
     public static void main(String[] args) {
         SpringApplication.run(OtpMobilApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        this.loggerService.error("Hello SpringBoot!");
     }
 }
--- otp-mobil/src/main/java/com/example/otpmobil/services/PaymentServiceImpl.java
+++ otp-mobil/src/main/java/com/example/otpmobil/services/PaymentServiceImpl.java
@@ -6,6 +6,8 @@ import com.example.otpmobil.models.Payment;
 import com.example.otpmobil.repositories.PaymentRepository;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -17,10 +19,13 @@ import java.util.List;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
+    private static final Logger logger = LoggerFactory.getLogger(PaymentServiceImpl.class);
     private final PaymentRepository paymentRepository;
+    private final LoggerService loggerService;
 
-    public PaymentServiceImpl(PaymentRepository paymentRepository) {
+    public PaymentServiceImpl(PaymentRepository paymentRepository, LoggerService loggerService) {
         this.paymentRepository = paymentRepository;
+        this.loggerService = loggerService;
     }
 
     public List<PaymentDTO> readBeansFromCsv(String path) throws IOException {
@@ -39,12 +44,11 @@ public class PaymentServiceImpl implements PaymentService {
         String pattern = "((18|19|20)[0-9]{2}[\\-.](0[13578]|1[02])[\\-.](0[1-9]|[12][0-9]|3[01]))|(18|19|20)[0-9]{2}[\\-.](0[469]|11)[\\-.](0[1-9]|[12][0-9]|30)|(18|19|20)[0-9]{2}[\\-.](02)[\\-.](0[1-9]|1[0-9]|2[0-8])|(((18|19|20)(04|08|[2468][048]|[13579][26]))|2000)[\\-.](02)[\\-.]29";
         for (PaymentDTO paymentDTO : beansFromCsv) {
             if (paymentDTO.getBankAccountNo().isBlank() && paymentDTO.getPaymentMode().equals("transfer")) {
-                System.out.println("hiányzó bankszámlaszám");
-                //logger.error("hiányzó bankszámlaszám");
+                logger.error("hiányzó bankszámlaszám");
             } else if (paymentDTO.getCardNo().isBlank() && paymentDTO.getPaymentMode().equals("card")) {
-                System.out.println("hiányzó bankkártyaszám");
+                logger.error("hiányzó bankkártyaszám " + paymentDTO);
             } else if (!(paymentDTO.getDate().matches(pattern))) {
-                System.out.println("nem megfelelő dátum formátum");
+                logger.error("nem megfelelő dátum formátum " + paymentDTO);
             } else {
                 validatedBeans.add(paymentDTO);
             }
